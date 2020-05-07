c2c5fb9b09d74065071c71d122a39b13bc5138c0
Szczepan Zalega <szczepan@nitrokey.com>
2020-05-07 17:00:30 +0200

    Correct pointer arithmetic

--- fido2/ctap_parse.c
+++ fido2/ctap_parse.c
@@ -1069,7 +1069,7 @@ static uint8_t parse_cred_mgmt_subcommandparams(CborValue * val, CTAP_credMgmt *
 
     const uint8_t * end_byte = cbor_value_get_next_byte(&map);
 
-    uint32_t length = (uint32_t)end_byte - (uint32_t)start_byte;
+    uint32_t length = end_byte - start_byte;
     if (length > sizeof(CM->hashed.subCommandParamsCborCopy))
     {
         return CTAP2_ERR_LIMIT_EXCEEDED;
