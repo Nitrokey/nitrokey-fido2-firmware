627e0f7c6bf4f0cd092e106c7c94232f1426b2c3
Szczepan Zalega <szczepan@nitrokey.com>
2020-08-29 14:37:49 +0200

    Decrease required UP time for extended/RESET operation

--- targets/stm32l432/src/gpio.h
+++ targets/stm32l432/src/gpio.h
@@ -34,7 +34,7 @@
 #define BUTTON_VALID_CONSUMED_T_MS    (10*1000)
 #define BUTTON_MAX_PRESS_T_MS    (3*1000)
 
-#define BUTTON_MIN_PRESS_T_MS_EXT    (10*1000)
+#define BUTTON_MIN_PRESS_T_MS_EXT    (5*1000)
 #define BUTTON_VALID_PRESS_T_MS_EXT    (2*1000)
 
 // set time after the power on, during which a single U2F or configuration
