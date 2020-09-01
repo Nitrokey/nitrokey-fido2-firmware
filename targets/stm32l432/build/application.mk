8a034f411968ae34ff541a2a16aa590f649f2b70
Szczepan Zalega <szczepan@nitrokey.com>
2020-09-01 16:11:30 +0200

    Build: remove extra -Og switch

--- targets/stm32l432/build/application.mk
+++ targets/stm32l432/build/application.mk
@@ -50,7 +50,7 @@ DEBUG ?= 0
 DEFINES = -DDEBUG_LEVEL=$(DEBUG) -D$(CHIP) -DAES256=1  -DUSE_FULL_LL_DRIVER -DAPP_CONFIG=\"app.h\" $(EXTRA_DEFINES)
 
 CFLAGS=$(INC) -c $(DEFINES)   -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -fdata-sections -ffunction-sections \
-	-fomit-frame-pointer $(HW) -g3 $(VERSION_FLAGS) -Og
+	-fomit-frame-pointer $(HW) -g3 $(VERSION_FLAGS)
 LDFLAGS_LIB=$(HW) $(SEARCH) -specs=nano.specs  -specs=nosys.specs  -Wl,--gc-sections -lnosys
 LDFLAGS=$(HW) $(LDFLAGS_LIB) -T$(LDSCRIPT) -Wl,-Map=$(TARGET).map,--cref -Wl,-Bstatic -ltinycbor
 
