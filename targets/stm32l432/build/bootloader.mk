9ff33de7a6278afd55ba8a14a68e2e11eeb1ddae
Szczepan Zalega <szczepan@nitrokey.com>
2021-06-26 12:26:35 +0200

    Add buildinfo file to release (2)

--- targets/stm32l432/build/bootloader.mk
+++ targets/stm32l432/build/bootloader.mk
@@ -47,7 +47,8 @@ DEFINES = -DDEBUG_LEVEL=$(DEBUG) -D$(CHIP) -DAES256=1  -DUSE_FULL_LL_DRIVER -DAP
 
 CFLAGS=$(INC) -c $(DEFINES)   -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -fdata-sections -ffunction-sections $(HW) -g $(VERSION_FLAGS)
 LDFLAGS_LIB=$(HW) $(SEARCH) -specs=nano.specs  -specs=nosys.specs  -Wl,--gc-sections  -lnosys
-LDFLAGS=$(HW) $(LDFLAGS_LIB) -T$(LDSCRIPT) -Wl,-Map=$(TARGET).map,--cref  -Wl,-Bstatic
+LDFLAGS_INFO=-Wl,--print-memory-usage -Wl,--print-gc-sections
+LDFLAGS=$(HW) $(LDFLAGS_LIB) -T$(LDSCRIPT) -Wl,-Map=$(TARGET).map,--cref  -Wl,-Bstatic $(LDFLAGS_INFO)
 
 
 .PRECIOUS: %.o
@@ -77,7 +78,7 @@ $(LDSCRIPT): $(LDSCRIPT).in
 	sed 's/__PAGES__/$(PAGES)/g' < $< >$@
 
 %.elf: $(OBJ)
-	$(CC) $^ $(HW) $(LDFLAGS) -o $@ -Wl,--print-memory-usage
+	$(CC) $^ $(HW) $(LDFLAGS) -o $@ 2>&1 | tee $(TARGET)-linking.buildinfo
 	$(SZ) $@
 
 %.hex: %.elf $(TARGET).buildinfo
